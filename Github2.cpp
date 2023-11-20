5647cec8890fd80035071fcda4b2e67e61e54994
Gustavo Mendes <gustavormendess@gmail.com>
2023-11-20 19:45:04 -0400

    Projeto Fluxo de Caixas

---
+++ Github2.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+//Nome da Impresa: a definir
+//Data de Início: 13/11
+//Data final: 24/11
+//Data da ultima Atualização: 20/11
+int main(void) {
+	float PagamentosDinheiro;
+	float VendasemDinheiro;
+	float RendimentoJuros;
+	float SaldoInicial;
+	float ContasdeClientes;
+	float Impostos;
+	float TotalRecibos;
+	float variacaoLiquida;
+	float TotalPagamentos;
+	float TotalFiscais;
+	int mes;
+	char nomeMes;
+	int opcao;
+	system("color F0");
+	system("cls");
+	printf("Escolha uma opcao a seguir: \n");
+	printf("1-Inserir: \n");
+	printf("2-Calcular Fluxo de caixa: \n");
+	scanf("%d", &opcao);
+	system("cls");
+}
+
